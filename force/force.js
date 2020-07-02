751cd16ce0d2df950cf5284c1d9ed697becb947f
Ching <p.ching0811@gmail.com>
2020-07-02 10:30:21 +0800

    group bubble

--- force/force.js
+++ force/force.js
@@ -5,20 +5,20 @@ var c5_color = d3.scaleQuantize()
 
 var color = d3.scaleQuantize()
     .domain([0,5])
-    .range(["#fff", "#5E4FA2", "#3288BD", "#66C2A5", "#ABDDA4", "#E6F598"]);
+    .range(["#040", "#5E4FA2", "#3288BD", "#66C2A5", "#ABDDA4", "#E6F598"]);
 
 var temp;
 
 file = "./final.csv"
 // file = "./t.csv"
-d3.csv(file, function(data){
+/*d3.csv(file, function(data){
     let margin = {top: 50, right: 50, bottom: 50, left: 50},
         width = 1*(window.screen.width), //- margin.left - margin.right,
         height = 1*(window.screen.height); //+ margin.top - margin.bottom;
 
     let maxRadius =  height*0.1,
         padding = 20,
-        clusterPadding = 10,
+        clusterPadding = 20,
         n = 218,
         clusters = new Array(5);
 
@@ -37,33 +37,42 @@ d3.csv(file, function(data){
     let radiusScale = d3.scaleLinear()
         .domain(d3.extent(data, function(d) { return (+d.valence);} ))
         // .range([1, 10]);
-        .range(d3.extent(data, function(d) { return (+d.valence)*3;} ));
+        .range(d3.extent(data, function(d) { return (+d.valence)*2;} ));
 
 
-    console.log(d3.extent(data, function(d) { return +d.valence;}));
+    // console.log(d3.extent(data, function(d) { return +d.valence;}));
 
-    console.log(data);
-    let nodes = data.map((d) => {
-        // scale radius to fit on the screen
-        let scaledRadius  = radiusScale(((+d.valence)*1)),
-            group = +d.C5_Modal-1;
+    // console.log(data);
+    
+    (generate_node(5, "valence"));
+    function generate_node(Cluster, Dimension){
 
-        // add cluster id and radius to array
-        d = {
-            cluster     : group,
-            r           : scaledRadius,
-            term        : d.Term,
-            valence     : +(d.valence)
-            // major_cat   : d.Major_category
-        };
-        // add to clusters array if it doesn't exist or the radius is larger than another radius in the cluster
-        if (!clusters[group] || (scaledRadius > clusters[group].r)) clusters[group] = d;
 
-        console.log(d);
-        
-        return d;
-    });
+        Dimension = "valence";
 
+         nodes = data.map((d) => {
+            // scale radius to fit on the screen
+            let scaledRadius  = radiusScale(((+d[Dimension])*1)),
+                group = +d.C5_Modal-1;
+
+            // add cluster id and radius to array
+            d = {
+                cluster     : group,
+                r           : scaledRadius,
+                term        : d.Term,
+                valence     : +(d[Dimension]),
+                // major_cat   : d.Major_category
+            };
+            // add to clusters array if it doesn't exist or the radius is larger than another radius in the cluster
+            if (!clusters[group] || (scaledRadius > clusters[group].r)) clusters[group] = d;
+
+            // console.log(d);
+
+            return d;
+        });
+
+        console.log(nodes);
+    }
 
 
     let circles = svg.append('g')
@@ -72,9 +81,11 @@ d3.csv(file, function(data){
         .data(d => d)
         .enter().append('circle')
         .attr('r', (d) => d.r)
+    // .transition()
+    // .duration(1000)
         .attr('fill', (d) => color(d.cluster))
         .attr('stroke', 'black')
-        .attr('stroke-width', 0.5)
+        .attr('stroke-width', 0)
         .call(d3.drag()
             .on("start", dragstarted)
             .on("drag", dragged)
@@ -96,12 +107,11 @@ d3.csv(file, function(data){
 
     // create the clustering/collision force simulation
     let simulation = d3.forceSimulation(nodes)
-        .velocityDecay(1)
-        // .force("x", d3.forceX().strength(.0005))
-        // .force("y", d3.forceY().strength(.0005))
+        .velocityDecay(0.2)
+        .force("x", d3.forceX().strength(.0005))
+        .force("y", d3.forceY().strength(.0005))
         .force("cluster", clustering)
         .force("collide", collide)
-
         .on("tick", ticked);
 
     function ticked() {
@@ -112,7 +122,7 @@ d3.csv(file, function(data){
 
     // Drag functions used for interactivity
     function dragstarted(d) {
-        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        if (!d3.event.active) simulation.alphaTarget(0.05).restart();
         d.fx = d.x;
         d.fy = d.y;
     }
@@ -127,7 +137,7 @@ d3.csv(file, function(data){
         d.fx = null;
         d.fy = null;
     }
-    
+
 
     // These are implementations of the custom forces.
     function clustering(alpha) {
@@ -148,6 +158,7 @@ d3.csv(file, function(data){
             }
         });
     }
+
 
     function collide(alpha) {
         var quadtree = d3.quadtree()
@@ -182,4 +193,177 @@ d3.csv(file, function(data){
     }
 
 
-});
+});*/
+
+// file = "./test.csv"
+
+var margin = {top: 50, right: 50, bottom: 50, left: 50},
+    w = 1*(window.screen.width), //- margin.left - margin.right,
+    h = 1*(window.screen.height); //+ margin.top - margin.bottom;
+
+var radius = 25;
+// var color = d3.scaleOrdinal(d3.schemeCategory20);
+var centerScale = d3.scalePoint().padding(1).range([0, w]);
+var forceStrength = 0.05;
+
+var svg = d3.select("#svg").append("svg")
+    .attr("width", w)
+    .attr("height", h)
+
+var simulation = d3.forceSimulation()
+    .force("collide",d3.forceCollide( function(d){
+        return d.r + 8 }).iterations(16) 
+    )
+    .force("charge", d3.forceManyBody())
+    .force("y", d3.forceY().y(h / 2))
+    .force("x", d3.forceX().x(w / 2))
+
+
+
+d3.csv(file, function(data){
+
+    Dimension = "valence";
+    Group = "C5_Modal";
+
+    data.forEach(function(d){
+        d.r = d[Dimension]*2;
+        d.x = w / 2;
+        d.y = h / 2;
+    })
+
+    console.table(data); 
+
+    var circles = svg.selectAll("circle")
+        .data(data, function(d){ return d.ID ;});
+
+    var circlesEnter = circles.enter().append("circle")
+        .attr("r", function(d, i){ return d.r; })
+        .attr("cx", function(d, i){ return 175 + 25 * i + 2 * i ** 2; })
+        .attr("cy", function(d, i){ return 250; })
+        .style("fill", function(d, i){ return color(d[Group]); })
+        .style("stroke", function(d, i){ return color(d[Group]); })
+        .style("stroke-width", 0)
+        .style("pointer-events", "all")
+        .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
+
+    circles = circles.merge(circlesEnter)
+
+    function ticked() {
+        circles
+            .attr("cx", function(d){ return d.x; })
+            .attr("cy", function(d){ return d.y; });
+    }   
+
+    simulation
+        .nodes(data)
+        .on("tick", ticked);
+
+    function dragstarted(d,i) {
+        //console.log("dragstarted " + i)
+        if (!d3.event.active) simulation.alpha(1).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
+
+    function dragged(d,i) {
+        //console.log("dragged " + i)
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    }
+
+    function dragended(d,i) {
+        //console.log("dragended " + i)
+        if (!d3.event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+        var me = d3.select(this)
+        console.log(me.classed("selected"))
+        me.classed("selected", !me.classed("selected"))
+
+        d3.selectAll("circle")
+            .style("fill", function(d, i){ return color(d.ID); })
+
+        d3.selectAll("circle.selected")
+            .style("fill", "none")
+
+    } 
+
+    function groupBubbles() {
+        hideTitles();
+
+        // @v4 Reset the 'x' force to draw the bubbles to the center.
+        simulation.force('x', d3.forceX().strength(forceStrength).x(w / 2));
+
+        // @v4 We can reset the alpha value and restart the simulation
+        simulation.alpha(1).restart();
+    }
+
+    function splitBubbles(byVar) {
+
+        centerScale.domain(data.map(function(d){ return d[byVar]; }));
+
+        if(byVar == "all"){
+            hideTitles()
+        } else {
+            showTitles(byVar, centerScale);
+        }
+
+        // @v4 Reset the 'x' force to draw the bubbles to their year centers
+        simulation.force('x', d3.forceX().strength(forceStrength).x(function(d){ 
+            return centerScale(d[byVar]);
+        }));
+
+        // @v4 We can reset the alpha value and restart the simulation
+        simulation.alpha(2).restart();
+    }
+
+    function hideTitles() {
+        svg.selectAll('.title').remove();
+    }
+
+    function showTitles(byVar, scale) {
+        // Another way to do this would be to create
+        // the year texts once and then just hide them.
+        var titles = svg.selectAll('.title')
+            .data(scale.domain());
+
+        titles.enter().append('text')
+            .attr('class', 'title')
+            .merge(titles)
+            .attr('x', function (d) { return scale(d); })
+            .attr('y', 40)
+            .attr('text-anchor', 'middle')
+            .text(function (d) { return byVar + ' ' + d; });
+
+        titles.exit().remove() 
+    }
+
+    function setupButtons() {
+        d3.selectAll('.button')
+            .on('click', function () {
+
+                // Remove active class from all buttons
+                d3.selectAll('.button').classed('active', false);
+                // Find the button just clicked
+                var button = d3.select(this);
+
+                // Set it as the active button
+                button.classed('active', true);
+
+                // Get the id of the button
+                var buttonId = button.attr('id');
+
+                console.log(buttonId)
+                // Toggle the bubble chart based on
+                // the currently clicked button.
+                splitBubbles(buttonId);
+            });
+    }
+
+    setupButtons();
+
+})
+
